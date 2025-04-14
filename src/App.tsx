9bb8f10c8bba3e1d661c9b3ec92245afabdfe50c
Clelton <cleltonjm_95@hotmail.com>
2025-04-14 12:26:39 -0300

    feat: Add new routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
+import { Router } from './Router'
 import { ThemeProvider } from 'styled-components'
+import { BrowserRouter } from 'react-router-dom'
 import { defaultTheme } from './styles/themes/default'
 import { GlobalStyle } from './styles/global'
 
 export default function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
   )
---
+++ src/Router.tsx
@@ -0,0 +1,14 @@
+import { Routes, Route } from 'react-router-dom'
+import { Home } from './pages/Home'
+import { SignIn } from './pages/SignIn'
+import { SignUp } from './pages/SignUp'
+
+export function Router() {
+    return (
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/sign-in" element={<SignIn />} />
+            <Route path="/sign-up" element={<SignUp />} />
+        </Routes>
+    )
+}
